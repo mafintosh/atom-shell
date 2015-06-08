554310bfef2ff56a38a25d71004688039fe8383b
Fabrice Weinberg <Fabrice@weinberg.me>
2015-06-08 13:11:20 +0200

    Allow configuration of electron version

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var fs = require('fs')
-var path = require('path')
+var os = require('os');
+var path = require('path');
 
-module.exports = fs.readFileSync(path.join(__dirname, 'path.txt'), 'utf-8')
+var paths = {
+  darwin: path.join(__dirname, './dist/Electron.app/Contents/MacOS/Electron'),
+  linux: path.join(__dirname, './dist/electron'),
+  win32: path.join(__dirname, './dist/electron.exe')
+};
+
+var electronPath = function(){
+  var platform = os.platform();
+  if (!paths[platform]) throw new Error('Unknown platform: ' + platform);
+  return paths[platform];
+};
+
+module.exports = electronPath();
--- install.js
+++ install.js
@@ -1,38 +1,30 @@
 #!/usr/bin/env node
 
 // maintainer note - update this manually when doing new releases:
-var version = '0.27.2'
 
-var fs = require('fs')
-var os = require('os')
-var path = require('path')
-var extract = require('extract-zip')
-var download = require('electron-download')
+var fs = require('fs');
+var path = require('path');
 
-var platform = os.platform()
+var extract = require('extract-zip');
+var download = require('electron-download');
+
+// default config
+var config = { version : '0.27.3' };
+
+// Try to load a custom electron config
+try { version = require('rc')('electron-prebuild', config); } catch (e) { }
 
 function onerror (err) {
-  throw err
+  throw err;
 }
-
-var paths = {
-  darwin: path.join(__dirname, './dist/Electron.app/Contents/MacOS/Electron'),
-  linux: path.join(__dirname, './dist/electron'),
-  win32: path.join(__dirname, './dist/electron.exe')
-}
-
-if (!paths[platform]) throw new Error('Unknown platform: ' + platform)
 
 // downloads if not cached
-download({version: version}, extractFile)
+download(config, extractFile);
 
 // unzips and makes path.txt point at the correct executable
 function extractFile (err, zipPath) {
-  if (err) return onerror(err)
-  fs.writeFile(path.join(__dirname, 'path.txt'), paths[platform], function (err) {
-    if (err) return onerror(err)
-    extract(zipPath, {dir: path.join(__dirname, 'dist')}, function (err) {
-      if (err) return onerror(err)
-    })
-  })
+  if (err) return onerror(err);
+  extract(zipPath, {dir: path.join(__dirname, 'dist')}, function (err) {
+    if (err) return onerror(err);
+  });
 }
