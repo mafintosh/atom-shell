45aaaaa8477be988a16d1d4536bc8db5e0856302
Max Goodman <c@chromakode.com>
2016-06-22 19:22:04 -0700

    Work around {stdio: 'inherit'} not working on Windows

--- cli.js
+++ cli.js
@@ -4,7 +4,9 @@ var electron = require('./')
 
 var proc = require('child_process')
 
-var child = proc.spawn(electron, process.argv.slice(2), {stdio: 'inherit'})
+var child = proc.spawn(electron, process.argv.slice(2))
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr)
 child.on('close', function (code) {
   process.exit(code)
 })
