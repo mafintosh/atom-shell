ab191c5272693b80e8fef2ee292e86fcc43263ef
Andrea Parodi <andrea@parro.it>
2016-07-30 01:55:13 +0200

    removed useless env argument passing

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ var proc = require('child_process')
 var env = process.env
 env.ELECTRON_ENV = env.ELECTRON_ENV || 'development'
 
-var child = proc.spawn(electron, process.argv.slice(2), {stdio: 'inherit', env: env})
+var child = proc.spawn(electron, process.argv.slice(2), {stdio: 'inherit'})
 child.on('close', function (code) {
   process.exit(code)
 })
