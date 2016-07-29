22f5b784709817eb7a1178919004f0e81f1506c2
Andrea Parodi <andrea@parro.it>
2016-07-30 01:59:43 +0200

    Add ELECTRON_ENV tests

---
+++ test/fixture/index.js
@@ -0,0 +1,2 @@
+console.log(process.env.ELECTRON_ENV)
+process.exit(0)
--- test/index.js
+++ test/index.js
@@ -3,6 +3,22 @@ var electron = require('../')
 var path = require('path')
 var pathExists = require('path-exists')
 var getHomePath = require('home-path')()
+var spawnSync = require('child_process').spawnSync
+
+tape('set ELECTRON_ENV to development if not defined', function (t) {
+  var args = [__dirname + '/fixture/index.js']
+  var result = spawnSync(__dirname + '/../cli.js', args).stdout.toString()
+  t.equal(result, 'development\n')
+  t.end()
+})
+
+tape('inherits ELECTRON_ENV from parent process is defined', function (t) {
+  var args = [__dirname + '/fixture/index.js']
+  process.env.ELECTRON_ENV = 'testing'
+  var result = spawnSync(__dirname + '/../cli.js', args).stdout.toString()
+  t.equal(result, 'testing\n')
+  t.end()
+})
 
 tape('has local binary', function (t) {
   t.ok(pathExists.sync(electron), 'electron was downloaded')
