f140b58f6b462181aa4804083f080ab3ff2f33c2
Andrea Parodi <andrea@parro.it>
2016-07-30 02:08:51 +0200

    removed spawnSync from tests

--- test/index.js
+++ test/index.js
@@ -3,21 +3,23 @@ var electron = require('../')
 var path = require('path')
 var pathExists = require('path-exists')
 var getHomePath = require('home-path')()
-var spawnSync = require('child_process').spawnSync
+var execFile = require('child_process').execFile
 
 tape('set ELECTRON_ENV to development if not defined', function (t) {
   var args = [__dirname + '/fixture/index.js']
-  var result = spawnSync(__dirname + '/../cli.js', args).stdout.toString()
-  t.equal(result, 'development\n')
-  t.end()
+  execFile(__dirname + '/../cli.js', args, function (err, stdout) {
+    t.equal(stdout.toString(), 'development\n')
+    t.end()
+  })
 })
 
 tape('inherits ELECTRON_ENV from parent process is defined', function (t) {
   var args = [__dirname + '/fixture/index.js']
   process.env.ELECTRON_ENV = 'testing'
-  var result = spawnSync(__dirname + '/../cli.js', args).stdout.toString()
-  t.equal(result, 'testing\n')
-  t.end()
+  execFile(__dirname + '/../cli.js', args, function (err, stdout) {
+    t.equal(stdout.toString(), 'testing\n')
+    t.end()
+  })
 })
 
 tape('has local binary', function (t) {
